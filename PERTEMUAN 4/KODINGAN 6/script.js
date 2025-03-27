56b5b8e926dbd8f1298b8a91f138a3e65309d59d
keissashafara <keissashafara79722@gmail.com>
2025-03-27 15:17:13 +0700

    Delete PERTEMUAN 4/KODINGAN 6 directory

--- PERTEMUAN 4/KODINGAN 6/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Aplikasi Gudang dan Penjualan telah dimuat!");
-    // Anda bisa tambahkan interaksi di sini, misalnya filter tabel
-});
